10167acb4005be74ecc83e802bbe56eb527c2326
bequshabeqa <bekatchitadze@gmail.com>
2026-06-23 20:53:41 +0400

    Added .gitignore and cleaned traking

--- web_Beka/core/models.py
+++ web_Beka/core/models.py
@@ -2,16 +2,6 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
 
-
-
-class Movie(models.Model):
-    title = models.CharField(max_length=255)
-    rate = models.FloatField()
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.title
-    
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
@@ -47,7 +37,6 @@ class Films(models.Model):
         decimal_places=1,   
         validators=[MinValueValidator(0.0), MaxValueValidator(10.0)]
     )
-    comment = models.TextField()
     link = models.URLField(null=True, blank=True)
 
     # 🌟 ახალი ველი მოწონებული ფილმებისთვის (Many-to-Many კავშირი)
@@ -69,3 +58,82 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.author.username} - {self.text[:20]}"
+
+
+
+
+
+
+
+
+# from django.db import models
+# from django.core.validators import MinValueValidator, MaxValueValidator
+# from django.contrib.auth.models import User
+
+
+
+# class Movie(models.Model):
+#     title = models.CharField(max_length=255)
+#     rate = models.FloatField()
+#     description = models.TextField(blank=True, null=True)
+
+#     def __str__(self):
+#         return self.title
+    
+# class Category(models.Model):
+#     name = models.CharField(max_length=100)
+#     description = models.TextField(null=True, blank=True)
+
+#     def __str__(self):
+#         return self.name
+
+#     @property
+#     def icon(self):
+#         genre_icons = {
+#             'Action': '💥',
+#             'Comedy': '😂',
+#             'კომედია': '😂',
+#             'Drama': '🎭',
+#             'Horror': '👻',
+#             'Sci-Fi': '👽',
+#             'Romance': '❤️',
+#             'Thriller': '🤫',
+#             'Fantasy': '🦄',
+#             'Animation': '🧸',
+#             'Documentary': '🎥',
+#         }
+#         return genre_icons.get(self.name, '🍿')
+
+
+# class Films(models.Model):
+#     title = models.CharField(max_length=200)
+#     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='films')
+#     description = models.TextField()
+#     release_date = models.DateField()
+#     rate = models.DecimalField(
+#         max_digits=3,      
+#         decimal_places=1,   
+#         validators=[MinValueValidator(0.0), MaxValueValidator(10.0)]
+#     )
+#     comment = models.TextField()
+#     link = models.URLField(null=True, blank=True)
+
+#     # 🌟 ახალი ველი მოწონებული ფილმებისთვის (Many-to-Many კავშირი)
+#     likes = models.ManyToManyField(User, related_name='liked_films', blank=True)
+
+#     def __str__(self):
+#         return self.title
+
+#     def get_absolute_url(self):
+#         return f"/product/{self.pk}/"
+
+
+# class Comment(models.Model):
+#     # 'film' არის კავშირი Films მოდელთან
+#     film = models.ForeignKey('Films', on_delete=models.CASCADE, related_name='comments')
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     text = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"{self.author.username} - {self.text[:20]}"
--- web_Beka/web_Beka/urls.py
+++ web_Beka/web_Beka/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for web_Beka project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from core.views import (
@@ -27,7 +11,9 @@ from core.views import (
     register_view,
     blog_view,
     contact_view,
+    FilmCreateView,
 )
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
@@ -36,7 +22,58 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),    
     path('product/<int:pk>/', product_view, name='product_detail'),
+    path('film/create/', FilmCreateView.as_view(), name='film_create'),
     path('comment/<int:comment_id>/delete/', delete_comment, name='delete_comment'),
     path('logout/', logout_view, name='logout'),
     path('contact/', contact_view, name='contact'),
 ]
+
+
+
+
+
+
+
+
+# """
+# URL configuration for web_Beka project.
+
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/6.0/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+# from django.contrib import admin
+# from django.urls import path
+# from core.views import (
+#     about_view, 
+#     home_view, 
+#     product_view, 
+#     delete_comment, 
+#     HomeView, 
+#     logout_view, 
+#     login_view,
+#     register_view,
+#     blog_view,
+#     contact_view,
+# )
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', HomeView.as_view(), name='home'),
+#     path('about/', about_view, name='about'),
+#     path('blog/', blog_view, name='blog'),
+#     path('login/', login_view, name='login'),
+#     path('register/', register_view, name='register'),    
+#     path('product/<int:pk>/', product_view, name='product_detail'),
+#     path('comment/<int:comment_id>/delete/', delete_comment, name='delete_comment'),
+#     path('logout/', logout_view, name='logout'),
+#     path('contact/', contact_view, name='contact'),
+# ]
